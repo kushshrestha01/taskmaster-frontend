ab35e61666294b56e172c2d716cc07a1621480a5
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-07-05 19:06:01 -0700

    mock.json data rendered in frontend

--- src/app.js
+++ src/app.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './app.scss';
+
+import mockData from './mock.json';
+console.log(mockData);
+
+function People(){
+  
+  const [people, setPeople] = useState([]);
+  const _getPeople = () =>{
+    setPeople(mockData)
+    //fetch from backend
+  };
+
+  useEffect(_getPeople,[]);
+  return(
+    <ul>
+      {people.map( (person) => 
+        <li key={person.id}>
+          <details>
+            <summary>
+              <span>Task Title: {person.title}</span>
+            </summary>
+            <div>
+              <span>Task Status: {person.status}</span>
+              <Description description={person.description}/>
+              <Assignee assignee={person.assignee}/>
+            </div>
+          </details>
+        </li>
+        )}
+    </ul>
+  )
+}
+
+function Description(props){
+  let description = props.description || [];
+  return(
+    <section>
+      <div>
+        <span>Description: {description}</span>
+      </div>
+    </section> 
+  )
+};
+
+function Assignee(props){
+  let assignee = props.assignee || [];
+  return(
+    <section>
+      <div>
+        <span>Assigned to: {assignee}</span>
+      </div>
+    </section> 
+  )
+};
+
+function Detail() {
+return(
+  <div>Task Details</div>
+)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>List of All Tasks Available in Database</header>
+      <main>
+        <People/>
+      </main>
+      <footer>copy 2019</footer>
+    </>
   );
 }
 
